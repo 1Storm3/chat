be0838c6a75d5a50d2cc3598023f3e0f79d39e04
1Storm3 <zemskoff2013@yandex.ru>
2023-12-29 20:18:51 +0300

    Update Messages.jsx

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,17 +1,54 @@
 import React from "react";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Messages.module.css";
 
-const Messages = ({ messages, name, time }) => {
+const Messages = ({ messages, name, time }, { socket }) => {
+  const [messageRecieved, setMessagesReceived] = useState([]);
+
+  const messagesColumnRef = useRef(null);
+
   const messagesRef = useRef(null);
+  useEffect(() => {
+    socket.on("receive_message", (newMessages) => {
+      setMessagesReceived((state) => [
+        ...state,
+        {
+          message: newMessages.message,
+          username: newMessages.username,
+          time: newMessages.timedata,
+        },
+      ]);
+    });
+    return () => socket.off("receive_message");
+  }, [socket]);
   const scrollToBottom = () => {
     messagesRef.current?.scrollIntoView({ behavior: "smooth" });
   };
+
+  useEffect(() => {
+    socket.on("last_100_messages", (last100Messages) => {
+      last100Messages = JSON.parse(last100Messages);
+      setMessagesReceived((state) => [...last100Messages, ...state]);
+    });
+
+    return () => socket.off("last_100_messages");
+  }, [socket]);
+
   useEffect(() => {
     scrollToBottom();
   }, [messages]);
+
   return (
-    <div className={styles.messages}>
+    <div className={styles.messages} ref={messagesColumnRef}>
+      {messageRecieved.map((msg, i) => (
+        <div key={i}>
+          <div>
+            <span>{msg.username}</span>
+            <span>{msg.message}</span>
+            <span>{msg.time}</span>
+          </div>
+        </div>
+      ))}
       {messages.map(({ user, message, time }, i) => {
         const itsMe =
           user.name.trim().toLowerCase() === name.trim().toLowerCase();
