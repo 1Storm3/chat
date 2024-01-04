b39b6e30e11d991d4ed6089ebda8256e565b29e8
1Storm3 <zemskoff2013@yandex.ru>
2024-01-04 22:18:56 +0300

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -21,7 +21,9 @@ const Register = () => {
           username,
           password,
         },
-        { headers: { "Access-Control-Allow-Origin": "*" } }
+        {
+          withCredentials: true,
+        }
       );
 
       if (response.status === 200) {
